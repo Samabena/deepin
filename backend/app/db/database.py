54b2bf243b5448bc3e8ea89e684a6a084dc26c14
KPALEMON Abena Samuel <sam@mm-dhcp-193-036.naist.jp>
2025-01-21 19:13:11 +0900

    final change

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,8 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql+psycopg2://postgres:qwerty@db/deap_innov"
+
+load_dotenv()
+
+# Get DATABASE_URL from environment variables
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
